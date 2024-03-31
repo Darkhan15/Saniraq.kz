e4e24cc20445f8243a70b0402ddb5497c4472be9
Darkhan15 <darkhan.tleubayev@mail.ru>
2024-04-01 02:24:03 +0500

    Add files via upload
    done

---
+++ saniraq/schemas.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+    name: str
+    city: str
+    phone: str
+
+class UserUpdate(BaseModel):
+    name: str
+    city: str
+    phone: str
+
+class AdCreate(BaseModel):
+    type: str
+    price: float
+    adress: str
+    area: float
+    rooms_count: int
+    description: str
+
+class CommentCreate(BaseModel):
+    content: str
+    created_at: datetime
+
+class CommentUpdate(BaseModel):
+    content: str
